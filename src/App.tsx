59ed60a1cc332a7d34809b0db0cff867e85ce217
Clare <chblackburne@gmail.com>
2020-07-02 20:01:46 +0100

    start using context

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,29 @@ import { AddNewItem } from "./components/AddNewItem";
 import { Card } from "./components/Card";
 import { AppContainer } from "./styles";
 
+interface State {
+  count: number;
+}
+
+type Action =
+  | {
+      type: "increment";
+    }
+  | {
+      type: "decrement";
+    };
+
+const counterReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
+
 function App() {
   return (
     <AppContainer>
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { AppStateProvider } from "./AppStateContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
